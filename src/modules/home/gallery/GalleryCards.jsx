7e6544c52f258c7e4689998a314a4083a7fde8a9
Kirby Rivera <kirbyrivera@Kirbys-Mac-mini.local>
2025-05-30 07:18:00 +0800

    FO-25 - Finished cloning the dribbble website.

--- src/modules/home/gallery/GalleryCards.jsx
+++ src/modules/home/gallery/GalleryCards.jsx
@@ -7,6 +7,17 @@ const GalleryCards = (props) => {
     <div className={styles["gal-card-main"]}>
       <div className={styles["gal-card-main-img-container"]}>
         <img src={image} alt="gallery canvas" />
+        <div className={styles["gal-card-main-overlay"]}>
+          <div className={styles["content"]}>{user}</div>
+          <div className={styles["content-container"]}>
+            <button className={styles["content-btn"]}>
+              <img src="assets/save-icon.svg" alt="" />
+            </button>
+            <button className={styles["content-btn"]}>
+              <img src="assets/heart.svg" alt="" />
+            </button>
+          </div>
+        </div>
       </div>
       <div className={styles["gal-card-main-content-container"]}>
         <div className={styles["gal-card-user"]}>
